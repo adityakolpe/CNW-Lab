f0b91add8d05a02a57cd3ee6da1e03cd430bd62e
adityakolpe <41753265+adityakolpe@users.noreply.github.com>
2020-01-31 15:30:30 +0530

    31/01/20

---
+++ 4lab/chatbot/server.c
@@ -0,0 +1,82 @@
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include<string.h>
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+
+void func(int sockfd) 
+{ 
+	do
+	{
+		char buff[50];
+		recv(sockfd,buff,sizeof(buff),0);
+		printf("Client: %s\n",buff);
+		if(strcmp(buff,"bye")==0) break;
+
+
+		scanf("%s",buff);
+		printf("Server: %s\n",buff );
+		send(sockfd,buff,sizeof(buff),0);
+		if(strcmp(buff,"bye")==0) break;
+	}while(1);
+} 
+
+// Driver function 
+int main() 
+{ 
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(PORT); 
+
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n"); 
+
+	// Now server is ready to listen and verification 
+	if ((listen(sockfd, 5)) != 0) { 
+		printf("Listen failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Server listening..\n"); 
+	len = sizeof(cli); 
+
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	if (connfd < 0) { 
+		printf("server acccept failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("server acccept the client...\n"); 
+
+	// Function for chatting between client and server 
+	func(connfd); 
+
+	// After chatting close the socket 
+	int cl = close(sockfd); 
+} 
